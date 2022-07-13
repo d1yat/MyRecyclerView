62b2b337eb894051f2a31c00a0a2071c8c76d9d6
apayah90 <apayah90@gmail.com>
2022-07-13 16:53:53 +0700

    sdfsd

--- app/src/main/java/com/example/myrecyclerview/ListHeroAdapter.kt
+++ app/src/main/java/com/example/myrecyclerview/ListHeroAdapter.kt
@@ -15,7 +15,7 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adap
     interface OnItemClickCallback {
         fun onItemClicked(data: Hero)
     }
-
+// test
     fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
         this.onItemClickCallback = onItemClickCallback
     }
